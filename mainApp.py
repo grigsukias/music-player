e86ed2f100e10b30d13edc6f6e054b586a91f864
Nomalizer <josephbates20051634@gmail.com>
2025-09-06 16:18:14 -0500

    "PPEGRK[lfadsmlmxcvz;]"

--- mainApp.py
+++ mainApp.py
@@ -1,4 +1,51 @@
 import sys
+import os
 import vlc
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QComboBox
 
+os.add_dll_directory(r"C:\Users\Joeyb\Documents\music player\music-player")
+
+class MusicPlayer(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Basic Music Player")
+        self.resize(800, 600)
+
+        self.label = QLabel("Title Name")
+        
+        self.label = QLabel("Select a song:")
+        self.dropdown = QComboBox()
+        self.load_songs()
+        
+        self.button = QPushButton("Play")
+        
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.button)
+        layout.addWidget(self.dropdown)
+        self.setLayout(layout)
+
+        self.instance = vlc.Instance()
+        self.player = self.instance.media_player_new()
+        self.button.clicked.connect(self.play_music)
+        
+    def load_songs(self):
+        songs_Path = os.path.dirname(os.path.abspath(__file__))
+        loadFolder = os.path.join(songs_Path, "testFiles")
+        
+        for file in  os.listdir(loadFolder):
+            if file.endswith((".mp3", ".wav", ".ogg")):
+                self.dropdown.addItem(os.path.join(loadFolder,file))
+
+    def play_music(self):
+        selected_item = self.dropdown.currentText()
+        media = self.instance.media_new(selected_item)
+        self.player.set_media(media)
+        self.player.play()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MusicPlayer()
+    window.show()
+    sys.exit(app.exec())
